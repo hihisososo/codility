95eb3cfa83e25e5cc09dd11ae65741824da6a1b1
lyj <lyj@diquest.com>
2022-06-14 09:15:22 +0900

    여러 연습문제 commit

---
+++ src/codility/CyclicRotation.java
@@ -0,0 +1,38 @@
+package codility;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] A = new int[]{3, 8, 9, 7, 6};
+        int k = 20;
+        System.out.println(Arrays.toString(new Solution().solution(A, k)));
+    }
+
+    public int[] solution(int[] A, int K) {
+        if(A.length == 0 || A.length == 1){
+            return A;
+        }
+        int mod = K % A.length;
+        int currIdx = 0;
+        int prevValue = A[0];
+        int orgIdxValue = 0;
+        for (int i = 0; i < A.length; i++) {
+            int rotateIdx = getRotateIdx(currIdx, mod, A.length);
+            orgIdxValue = A[rotateIdx];
+            A[rotateIdx] = prevValue;
+            prevValue = orgIdxValue;
+            currIdx = rotateIdx;
+        }
+
+        return A;
+    }
+
+    private int getRotateIdx(int currIdx, int rotateSize, int length) {
+        if (currIdx + rotateSize >= length) {
+            return (currIdx + rotateSize) - length;
+        } else {
+            return currIdx + rotateSize;
+        }
+    }
+}
---
+++ src/codility/OddOccurrencesInArray.java
@@ -0,0 +1,25 @@
+package codility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] A = new int[]{9, 3, 9, 3, 9, 7, 9};
+        System.out.println(new Solution().solution(A));
+    }
+
+    public int solution(int[] A) {
+        HashMap<Integer, Integer> cntMap = new HashMap<>();
+        for (int i : A) {
+            cntMap.merge(i, 1, Integer::sum);
+        }
+        for (Map.Entry<Integer, Integer> e : cntMap.entrySet()) {
+            if (e.getValue() % 2 == 1) {
+                return e.getKey();
+            }
+        }
+        return -1;
+    }
+}
+
